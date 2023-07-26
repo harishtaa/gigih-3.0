2b91eaa1a9491c6b01d890af3e6889a6fcf1b3ce
Harish Adityawan <haradityawan@gmail.com>
2023-07-26 11:18:34 +0700

    add module 4.1 react-basic

---
+++ 4.1 - react-basic/react-gigih/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Avatar from './components/Avatar'
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Avatar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 4.1 - react-basic/react-gigih/src/components/Avatar.js
@@ -0,0 +1,19 @@
+const user = 
+    
+    {
+        name: "Shani Indira Natio",
+        middleName: "Shani",
+        image: "https://img.celebrities.id/okz/600/9BaU18/master_J9l38BQ8u6_482_shani_jkt48.jpg"
+    }
+const fullName = `${user.name.split(" ")[0]} "${user.middleName}" ${user.name.replace(user.name.split(" ")[0],"")}`
+
+const Avatar = () => {
+    return(
+        <div>
+            <img src = {user.image} className = "avatar"></img>
+            <h1>{fullName}</h1>
+        </div>
+    )
+}
+
+export default Avatar
